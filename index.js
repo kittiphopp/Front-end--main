3793ac7a0fc4037ea1fef1c890f8eb2954c6a37c
kittiphopp <parkpor26@gmail.com>
2024-02-21 10:41:49 +0700

    .

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var bodyParser = require("body-parser");
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "http://10.104.16.46:3000";
-//const base_url = "http://localhost:3000";
+//const base_url = "http://10.104.16.46:3000";
+const base_url = "http://localhost:3000";
 //const base_url = "http://node41091-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
